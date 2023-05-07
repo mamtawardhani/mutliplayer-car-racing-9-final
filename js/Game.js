4d7f60d4a812aa4564fc1ddea8fe56f32b2b505e
mamtawardhani <53176352+mamtawardhani@users.noreply.github.com>
2023-05-07 15:00:09 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -202,14 +202,7 @@ handleResetButton() {
 }
 
 showLife() {
-  push();
-  image(lifeImage, width / 2 - 130, height - player.positionY - 400, 20, 20);
-  fill("white");
-  rect(width / 2 - 100, height - player.positionY - 400, 185, 20);
-  fill("#f50057");
-  rect(width / 2 - 100, height - player.positionY - 400, player.life, 20);
-  noStroke();
-  pop();
+ 
 }
 showLeaderboard() {
   var leader1, leader2;
